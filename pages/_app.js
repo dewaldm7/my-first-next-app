20eb74019f67614dc71359fedc900fd3e3514e82
Dewald <dewaldm7@gmail.com>
2022-11-29 17:28:36 +0200

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,33 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import Script from "next/script";
 
 //Layout component wraps around MyApp component
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-334KVN4N66"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-334KVN4N66', {
+page_path: window.location.pathname,
+});
+`,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
 
